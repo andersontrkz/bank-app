0480dd651165c48bd66a9c01a6ebeff8eda69b22
Anderson <anderson.turkiewicz@intmed.com.br>
2023-06-14 20:18:52 -0300

    feat: add custom header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,15 +1,47 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { FontAwesome } from '@expo/vector-icons'; 
+
+const statusBarHeight = StatusBar.currentHeight ? StatusBar.currentHeight + 24 : 72;
 
 export default function Header() {
   return (
     <View style={styles.container}>
-      <Text>Header</Text>
+      <View style={styles.content}>
+        <Text style={styles.username}>Anderson Luiz</Text>
+
+        <TouchableOpacity style={styles.userIcon}>
+          <FontAwesome  name="user-o" size={28} color="black" />
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-    container: {
+  container: {
     backgroundColor: '#ffce00',
+    flexDirection: 'row',
+    paddingVertical: statusBarHeight ,
+    paddingHorizontal: 24,
   },
+  content: {
+    alignItems: 'center',
+    backgroundColor: '#ffce00',
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingTop: statusBarHeight,
+  },
+  username: {
+    fontSize: 18,
+    fontWeight: '700',
+  },
+  userIcon: {
+    alignItems: 'center',
+    backgroundColor: 'rgba(255, 255, 255, 0.5)',
+    borderRadius: 48/2,
+    height: 48,
+    justifyContent: 'center',
+    width: 48,
+  }
 });
