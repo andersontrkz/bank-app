a9d69c4b757326cc76bd7390b6cd6bdfd5530fc2
Anderson <anderson.turkiewicz@intmed.com.br>
2023-06-15 00:18:50 -0300

    feat: add animated header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,5 +1,6 @@
-import { StatusBar, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { StatusBar, StyleSheet, TouchableOpacity, View } from 'react-native';
 import { FontAwesome } from '@expo/vector-icons';
+import { MotiView, MotiText } from 'moti';
 import User from '../../interfaces/User';
 
 const statusBarHeight = StatusBar.currentHeight ? StatusBar.currentHeight + 24 : 72;
@@ -11,13 +12,42 @@ type HeaderProps = {
 export default function Header({ user: { name } }: HeaderProps) {
   return (
     <View style={styles.container}>
-      <View style={styles.content}>
-        <Text style={styles.username}>{name}</Text>
-
+      <MotiView style={styles.content} 
+      from={{
+        translateY: -160,
+        opacity: 0,
+      }}
+      animate={{
+        translateY: 0,
+        opacity: 1,
+      }}
+      transition={{
+        type: 'timing',
+        duration: 240,
+        delay: 240,
+      }}
+      >
+        <MotiText style={styles.username}
+          from={{
+            translateX: -240,
+            opacity: 0,
+          }}
+          animate={{
+            translateX: 0,
+            opacity: 1,
+          }}
+          transition={{
+            type: 'timing',
+            duration: 480,
+            delay: 480,
+          }}
+        >
+          {name}
+        </MotiText>
         <TouchableOpacity style={styles.userIcon} activeOpacity={0.5}>
           <FontAwesome  name="user-o" size={28} color="black" />
         </TouchableOpacity>
-      </View>
+      </MotiView>
     </View>
   );
 }
