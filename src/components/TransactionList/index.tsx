0032e552ede74409d2b2ab06a21cb9fd27f350f0
Anderson <anderson.turkiewicz@intmed.com.br>
2023-06-15 23:00:37 -0300

    feat: eslint

--- src/components/TransactionList/index.tsx
+++ src/components/TransactionList/index.tsx
@@ -1,4 +1,4 @@
-import {FlatList, ScrollView, Text} from 'react-native';
+import {FlatList, Text} from 'react-native';
 import TransactionItem from '../../components/TransactionItem';
 import type Transaction from '../../interfaces/Transaction';
 import styles from './styles';
@@ -9,15 +9,17 @@ type TransactionProps = {
 
 export default function TransactionList({transactions}: TransactionProps) {
 	return (
-		<ScrollView showsVerticalScrollIndicator={false}>
+		<>
 			<Text style={styles.title}>Últimas movimentações</Text>
 
 			<FlatList
+				scrollEnabled
+				showsVerticalScrollIndicator={false}
 				style={styles.transactions}
 				data={transactions}
 				keyExtractor={ transaction => String(transaction.id) }
 				renderItem={ ({item}) => <TransactionItem transaction={item} /> }
 			/>
-		</ScrollView>
+		</>
 	);
 }
